72ee7292220efe42826b9c20160417aff56bb85a
Joko Eliyanto <57888234+jokoeliyanto@users.noreply.github.com>
2023-01-22 09:06:41 +0700

    Add files via upload

---
+++ Membuat Database Film Indonesia/genre.sql
@@ -0,0 +1,23 @@
+CREATE TABLE genre(
+   id   INTEGER  NOT NULL PRIMARY KEY 
+  ,nama VARCHAR(12) NOT NULL
+);
+INSERT INTO genre(id,nama) VALUES (12,'Petualangan');
+INSERT INTO genre(id,nama) VALUES (14,'Fantasi');
+INSERT INTO genre(id,nama) VALUES (16,'Animasi');
+INSERT INTO genre(id,nama) VALUES (18,'Drama');
+INSERT INTO genre(id,nama) VALUES (27,'Kengerian');
+INSERT INTO genre(id,nama) VALUES (28,'Aksi');
+INSERT INTO genre(id,nama) VALUES (35,'Komedi');
+INSERT INTO genre(id,nama) VALUES (36,'Sejarah');
+INSERT INTO genre(id,nama) VALUES (37,'Barat');
+INSERT INTO genre(id,nama) VALUES (53,'Cerita Seru');
+INSERT INTO genre(id,nama) VALUES (80,'Kejahatan');
+INSERT INTO genre(id,nama) VALUES (99,'Dokumenter');
+INSERT INTO genre(id,nama) VALUES (878,'Cerita Fiksi');
+INSERT INTO genre(id,nama) VALUES (9648,'Misteri');
+INSERT INTO genre(id,nama) VALUES (10402,'Musik');
+INSERT INTO genre(id,nama) VALUES (10749,'Percintaan');
+INSERT INTO genre(id,nama) VALUES (10751,'Keluarga');
+INSERT INTO genre(id,nama) VALUES (10752,'Kejahatan');
+INSERT INTO genre(id,nama) VALUES (10770,'Film TV');
---
+++ Membuat Database Film Indonesia/negara.sql
@@ -0,0 +1,27 @@
+CREATE TABLE negara(
+   id   VARCHAR(2) NOT NULL PRIMARY KEY
+  ,nama VARCHAR(24) NOT NULL
+);
+INSERT INTO negara(id,nama) VALUES ('AU','Australia');
+INSERT INTO negara(id,nama) VALUES ('BE','Belgium');
+INSERT INTO negara(id,nama) VALUES ('CN','China');
+INSERT INTO negara(id,nama) VALUES ('DE','Germany');
+INSERT INTO negara(id,nama) VALUES ('DK','Denmark');
+INSERT INTO negara(id,nama) VALUES ('FR','France');
+INSERT INTO negara(id,nama) VALUES ('GB','United Kingdom');
+INSERT INTO negara(id,nama) VALUES ('HK','Hong Kong');
+INSERT INTO negara(id,nama) VALUES ('ID','Indonesia');
+INSERT INTO negara(id,nama) VALUES ('JP','Japan');
+INSERT INTO negara(id,nama) VALUES ('KR','South Korea');
+INSERT INTO negara(id,nama) VALUES ('MY','Malaysia');
+INSERT INTO negara(id,nama) VALUES ('NL','Netherlands');
+INSERT INTO negara(id,nama) VALUES ('NO','Norway');
+INSERT INTO negara(id,nama) VALUES ('NZ','New Zealand');
+INSERT INTO negara(id,nama) VALUES ('PH','Philippines');
+INSERT INTO negara(id,nama) VALUES ('PL','Poland');
+INSERT INTO negara(id,nama) VALUES ('QA','Qatar');
+INSERT INTO negara(id,nama) VALUES ('SG','Singapore');
+INSERT INTO negara(id,nama) VALUES ('TH','Thailand');
+INSERT INTO negara(id,nama) VALUES ('TW','Taiwan');
+INSERT INTO negara(id,nama) VALUES ('US','United States of America');
+INSERT INTO negara(id,nama) VALUES ('ZA','South Africa');
